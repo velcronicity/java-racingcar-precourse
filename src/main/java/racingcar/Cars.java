1e7d6ac72d76cc7ecdd7e739c26b0237c658e146
velcronicity <velcronicity@gmail.com>
2022-10-05 18:15:26 +0900

    feat: 전진멈춤
    자동차는 랜덤값에 따라 전진 또는 멈춤한다

---
+++ src/main/java/racingcar/Cars.java
@@ -0,0 +1,13 @@
+package racingcar;
+
+import static racingcar.Message.*;
+
+public class Cars {
+    public GoStop determineGoStop(int randomNumber) {
+        if (randomNumber < 0 || randomNumber > 9)
+            throw new IllegalStateException(RANDOM_NUMBER_ERROR);
+        if (randomNumber >= 4)
+            return GoStop.GO;
+        return GoStop.STOP;
+    }
+}
